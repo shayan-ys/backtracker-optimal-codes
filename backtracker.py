3ea003295f6c9304c8cc6419dfa1c3f6f3bc12a2
Shayan Yousefian <shayan.yousefian1372@gmail.com>
2017-11-21 13:43:56 -0500

    New algorithm, from 1P02 Course lecture

--- backtracker.py
+++ backtracker.py
@@ -8,10 +8,8 @@ PRINT_BENCHMARKS = True
 Version 2.1
 Instead of writing the Hamming distance in the table, just a boolean which says it's >= d or not.
 Benchmark:
-n=10, d=4: 0:00:03.120381 - with wrong result: 36, correct is 40
-n=11, d=4: 0:00:21.742692 - correct answer: 72
-n=12, d=4: 0:03:10.026099 - correct answer: 144
-n=13, d=4: 0:34:11.182061 - correct answer: 256
+n=8,  d=4: 0:00:00.050291 - correct answer: 16
+n=9,  d=4: 0:01:16.085821 - correct answer: 20
 """
 
 
@@ -19,13 +17,14 @@ def hamming_distance(vector_1: np.array, vector_2: np.array) -> int:
     return np.count_nonzero(vector_1 != vector_2)
 
 
-def generate_all_vectors(length: int) -> list:
+def generate_all_vectors(length: int, q: int=2) -> list:
     """
     Creates all possible binary numbers with length n, and casts them to np.array
     :param length: n
+    :param q: value for GF, if 2 binary codes only 0 or 1 are valid bits
     :return: list of np.arrays representing all possible binary numbers of length n
     """
-    return list(map(np.array, itertools.product([0, 1], repeat=length)))
+    return list(map(np.array, itertools.product(list(range(q)), repeat=length)))
 
 
 def generate_hamming_distance_table(vector_list: list, minimum_distance: int, print_result: bool=False) -> list:
@@ -34,6 +33,7 @@ def generate_hamming_distance_table(vector_list: list, minimum_distance: int, pr
     Based on if they satisfy the given minimum distance or not.
     :param vector_list: List of vectors
     :param minimum_distance: Each two vectors that are 'minimum_distance' away from each other will be flagged as 'True'
+    :param print_result: Print the table in the end.
     :return: Hamming distance of vectors (in order with integer indexes)
     """
     global PRINT_BENCHMARKS
@@ -76,51 +76,67 @@ def is_word_satisfy_minimum_distance_of_code(code: list, hamming_distance_list_f
     return True
 
 
-def backtrack(code: list, candidates: list, level: int=1) -> list:
-    global hamming_distance_table, codes_list
+def backtrack(code: list, candidates: list, level: int=0) -> (int, list):
+    global hamming_distance_table, codes_list, promised_M, leading_bit_non_zero, q
 
-    for lexi_index, word in enumerate(candidates):
+    for lexi_index, word in enumerate(candidates[level]):
 
         hamming_distance_list_for_word = hamming_distance_table[word]
 
-        if level == 1:
+        if level == 0:
             code = [word]
         else:
-            if not is_word_satisfy_minimum_distance_of_code(code, hamming_distance_list_for_word):
-                continue
-            code.append(word)
+            if len(code) <= level:
+                code.append(word)
+            else:
+                code[level] = word
 
-        candidates_for_current_word = []
-        for candidate_for_word in candidates[lexi_index:]:
+        if not leading_bit_non_zero[word] and level >= (promised_M / q):
+            codes_list.append(code)
+            return level + 1, code
+
+        if level + 1 >= promised_M:
+            codes_list.append(code)
+            return level + 1, code
+
+        if len(candidates) <= level + 1:
+            candidates.append([])
+        else:
+            candidates[level + 1] = []
+
+        for candidate_for_word in candidates[level][lexi_index:]:
 
             if hamming_distance_list_for_word[candidate_for_word]:
-                candidates_for_current_word.append(candidate_for_word)
+                candidates[level + 1].append(candidate_for_word)
 
-        if not candidates_for_current_word:
-            continue
-
-        backtrack(code, candidates_for_current_word, level+1)
-
-        if level == 1:
+        if level + 1 + len(candidates[level + 1]) < promised_M:
             codes_list.append(code)
+            return level + 1, code
 
-    if level == 1 and not codes_list:
-        codes_list.append(code)
+        found_level, found_code = backtrack(code, candidates, level+1)
 
-    return code
+        if found_level >= promised_M:
+            return found_level, found_code
+
+    return level
 
 
 timer = datetime.now()
+q = 2
 
 n = 9
-d = 4
+d = 3
+promised_M = 40
 
 """
 Generates all vectors and sort them by their weight -> all possible binary numbers in lexicographical order
 """
-vectors = sorted(generate_all_vectors(n), key=np.count_nonzero)
+vectors = sorted(generate_all_vectors(n, q), key=np.count_nonzero)
 
-# print(vectors)
+leading_bit_non_zero = {lexi_index: (vector[0] != 0) for lexi_index, vector in enumerate(vectors)}
+# print(leading_bit_non_zero)
+
+print([str(i) + ': ' + ''.join(map(str, vector)) for i, vector in enumerate(vectors)])
 
 """
 Pre-Computing hamming distance satisfaction table, just store if two vectors have a distance more than d or not.
@@ -130,17 +146,18 @@ hamming_distance_table = generate_hamming_distance_table(vectors, d)
 codes_list = []
 init_candidates = list(range(len(vectors)))     # list of vectors indexes from 'vectors' lexi-sorted list.
 
-backtrack([], init_candidates)
+max_found_M, best_code_vector_indexes = backtrack([], [init_candidates])
 
-max_found_M = 0
-best_code_vector_indexes = []
-for found_code in codes_list:
-
-    if len(found_code) > max_found_M:
-        best_code_vector_indexes = found_code
-        max_found_M = len(best_code_vector_indexes)
+# max_found_M = 0
+# best_code_vector_indexes = []
+# for found_code in codes_list:
+#
+#     if len(found_code) > max_found_M:
+#         best_code_vector_indexes = found_code
+#         max_found_M = len(best_code_vector_indexes)
+print('=== For n=' + str(n) + ' and d=' + str(d) + ' in GF(' + str(q) + '):')
+print('max found M is: ' + str(max_found_M))
+print('code is: ' + str([''.join(map(str, vectors[i])) for i in best_code_vector_indexes]))
 
 if PRINT_BENCHMARKS:
     print('----------------------- process took: ' + str(datetime.now() - timer) + ' time ----')
-print('max found M is: ' + str(max_found_M))
-print('code is: ' + str([''.join(map(str, vectors[i])) for i in best_code_vector_indexes]))
